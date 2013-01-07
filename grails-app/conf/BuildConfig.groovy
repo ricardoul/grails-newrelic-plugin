913abd88bb6cb383406f23642326aad0954d93d5
CP Lim <cheenpin.lim@gmail.com>
2013-01-07 22:41:41 +1100

    Updated to v2.12.0 of NewRelic API

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        compile "newrelic.java-agent:newrelic-api:2.9.0"
+        compile "newrelic.java-agent:newrelic-api:2.12.0"
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
